f247d5f0ee46799538e277c53c08f0d856faacee
slazarska <slazarska@protonmail.com>
2022-11-27 15:36:15 +0300

    deleted one test, changed 2 tests

--- text_example.py
+++ text_example.py
@@ -1,11 +1,8 @@
-def test_example_0():
+def test_example_1():
     assert 3 > 1
     assert 3 > 2
 
 
-def test_example_1():
-    assert 3 > 1
-
-
 def test_example_2():
-    assert 0 == 0
+    assert 4 > 3
+    assert 4 > 2
