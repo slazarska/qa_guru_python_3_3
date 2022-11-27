5603090d3badd4a2c9a3a6577ece6ca95fc178ed
slazarska <slazarska@protonmail.com>
2022-11-27 15:45:42 +0300

    reverted

--- text_example.py
+++ text_example.py
@@ -1,8 +1,11 @@
-def test_example_1():
+def test_example_0():
     assert 3 > 1
     assert 3 > 2
 
 
+def test_example_1():
+    assert 3 > 1
+
+
 def test_example_2():
-    assert 4 > 3
-    assert 4 > 2
+    assert 0 == 0
